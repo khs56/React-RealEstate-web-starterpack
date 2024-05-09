41998142e6dc6b016a047050bb900e81cff69880
Vishal M <48333506+khs56@users.noreply.github.com>
2024-05-09 11:57:20 -0400

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,10 +4,10 @@ import './Header.css'
 const Header = () => {
   return (
     <section className="h-wrapper">
-      <div className="flexCenter paddings innerWidth">
+      <div className="h-container flexCenter paddings innerWidth">
         <img src="./logo.png" alt="logo" width={100} />
 
-        <div className="h-menu">
+        <div className="flexCenter h-menu">
           <a href="">Residencies</a>
           <a href="">Our Values</a>
           <a href="">Contact Us</a>
