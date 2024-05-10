29a93f2cdf47ecfeb43ef2a3adef9b6c3f995ae2
Vishal M <48333506+khs56@users.noreply.github.com>
2024-05-10 14:04:53 -0400

    Update Residencies.jsx

--- src/components/Residencies/Residencies.jsx
+++ src/components/Residencies/Residencies.jsx
@@ -3,6 +3,7 @@ import { Swiper, SwiperSlide, useSwiper } from 'swiper/react'
 import "swiper/css"
 import './Residencies.css'
 import data from '../../utils/slider.json'
+import { sliderSettings } from "../../utils/common"
 
 function Residencies() {
   return (
@@ -12,6 +13,24 @@ function Residencies() {
           <span className='orangeText'>Best Choices</span>
           <span className='primaryText'>Popular Residencies</span>
         </div>
+
+        <Swiper {...sliderSettings}>
+          {
+            data.map((card, i) => (
+              <SwiperSlide key={i}>
+                <div className="flexColStart r-card">
+                  <img src={card.image} alt="home" />
+                  <span className="secondaryText r-pice">
+                    <span style={{ color: "orange" }}>$</span>
+                    <span>{card.price}</span>
+                  </span>
+
+                  <span className='primaryText'>{card.name}</span>
+                  <span className='secondaryText'>{card.name}</span>
+                </div>
+              </SwiperSlide>
+            ))}
+        </Swiper>
       </div>
     </section>
   )
