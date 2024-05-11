30fead3e0226707480275ce61575356ff672e834
Vishal M <48333506+khs56@users.noreply.github.com>
2024-05-10 22:13:00 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Companies from './components/Companies/Companies'
 import './App.css'
 import Residencies from './components/Residencies/Residencies';
 import Value from './components/Value/Value'
+import Contact from './components/Contact/Contact'
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Companies />
         <Residencies />
         <Value />
+        <Contact />
       </div>
     </div>
   );
